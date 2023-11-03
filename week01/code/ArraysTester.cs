81e0610c51ab7ff2e7faf0e4e56e938b7941c80e
Wards <matt.19076@hotmail.com>
2023-11-02 21:57:32 -0500

    hi

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -34,12 +34,16 @@ public static class ArraysTester {
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        double[] multiples; 
+        double multiplier = 1;
+        for (int i = 0; i < length; i ++)
+        {
+            double times = number * multiplier;
+            multiples[i] = times;
+            multiplier ++;
+        }
 
-        return new double[0]; // replace this return statement with your own
+        return multiples;
     }
     
     /// <summary>
@@ -52,10 +56,8 @@ public static class ArraysTester {
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
+        int first = data[0];
+        data.RemoveAt(0);
+        data.Add(first);
     }
 }
