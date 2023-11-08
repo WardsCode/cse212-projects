83a02ba294d14d129771f7405ccd601db075412a
Wards <matt.19076@hotmail.com>
2023-11-07 22:42:19 -0500

    m

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -76,8 +76,8 @@ public class SimpleQueue {
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
