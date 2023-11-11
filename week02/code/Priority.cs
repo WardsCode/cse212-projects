939c837aaf34bd12b932a974726af48a68dd77bd
Wards <matt.19076@hotmail.com>
2023-11-11 11:34:18 -0500

    added second part

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -18,20 +18,35 @@ public static class Priority {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: I add 4 people to the Q and then dequeue 1 according to their priority
+        // Expected Result: Retreive the one that is highest priority and then the next one
         Console.WriteLine("Test 1");
 
-        // Defect(s) Found: 
+        priorityQueue.Enqueue("Wards", 1);
+        priorityQueue.Enqueue("Seth", 3);
+        priorityQueue.Enqueue("Matt", 5);
+        priorityQueue.Enqueue("Ethan", 2);
+        Console.WriteLine(priorityQueue);
+        for (int i = 0; i < 4 ; i++ ){
+        Console.WriteLine("Next in turn for priority is " + priorityQueue.Dequeue());
+        }
+        // Defect(s) Found: it does not dequeue
 
         Console.WriteLine("---------");
 
         // Test 2
         // Scenario: 
-        // Expected Result: 
+        // Expected Result: it will arrange fifo on equal priority
         Console.WriteLine("Test 2");
 
-        // Defect(s) Found: 
+        priorityQueue.Enqueue("Wards", 5);
+        priorityQueue.Enqueue("Seth", 3);
+        priorityQueue.Enqueue("Matt", 5);
+        priorityQueue.Enqueue("Ethan", 2);
+        Console.WriteLine(priorityQueue);
+        for (int i = 0; i < 4 ; i++ ){
+        Console.WriteLine("Next in turn for priority is " + priorityQueue.Dequeue());
+        }
 
         Console.WriteLine("---------");
 
